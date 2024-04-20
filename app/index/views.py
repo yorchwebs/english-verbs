24496ae396f0e9371b9a4d6baef994c4b916902c
JorgeGarcia_Dev <georgepromotions@gmail.com>
2024-04-19 22:07:27 -0600

    refactor: the index view

--- app/index/views.py
+++ app/index/views.py
@@ -1,5 +1,4 @@
 """This module contains the views for the index page."""
-from flask import request
 from flask import Response
 from flask import Blueprint
 from flask import render_template
@@ -19,45 +18,25 @@ index_bp = Blueprint(
 
 @index_bp.route("/", methods=["GET", "POST"])
 def index() -> Response:
-    """Render the index page and handle form submission."""
+    """Handle index page and form submission."""
     form: VerbForm = VerbForm()
-    if request.method == "POST" and form.validate_on_submit():
-        # If the form is submitted and valid,
-        # insert the verb and retrieve matching verbs
-        insert_verb: VerbFormModel = VerbFormModel(verb=form.verb.data)
-        if insert_verb.verb:
-            verbs: List[str] = Verb.select().where(
-                (Verb.simple_form.contains(insert_verb.verb))
-                | (Verb.third_person.contains(insert_verb.verb))
-                | (Verb.simple_past.contains(insert_verb.verb))
-                | (Verb.past_participle.contains(insert_verb.verb))
-                | (Verb.gerund.contains(insert_verb.verb))
-                | (Verb.meaning.contains(insert_verb.verb))
+    if form.validate_on_submit():
+        # Insert verb and retrieve matching verbs
+        verb: VerbFormModel = form.verb.data
+        if verb:
+            matching_verbs: List[Verb] = Verb.select().where(
+                (Verb.simple_form.contains(verb))
+                | (Verb.third_person.contains(verb))
+                | (Verb.simple_past.contains(verb))
+                | (Verb.past_participle.contains(verb))
+                | (Verb.gerund.contains(verb))
+                | (Verb.meaning.contains(verb))
             )
         else:
-            # If the form is submitted but the verb field is empty,
-            # retrieve all verbs
-            verbs: List[str] = Verb.select(
-                Verb.num_id,
-                Verb.verb_type,
-                Verb.simple_form,
-                Verb.third_person,
-                Verb.simple_past,
-                Verb.past_participle,
-                Verb.gerund,
-                Verb.meaning,
-            )
-        return render_template("index.html", form=form, verbs=verbs)
-    # If the request method is GET, retrieve all verbs and render the
-    # index page
-    all_verbs: List[str] = Verb.select(
-        Verb.num_id,
-        Verb.verb_type,
-        Verb.simple_form,
-        Verb.third_person,
-        Verb.simple_past,
-        Verb.past_participle,
-        Verb.gerund,
-        Verb.meaning,
-    )
-    return render_template("index.html", form=form, verbs=all_verbs)
+            # Retrieve all verbs if form is submitted but verb field is empty
+            matching_verbs = Verb.select()
+        return render_template("index.html", form=form, verbs=matching_verbs)
+    # Retrieve all verbs if request method is GET
+    verbs = Verb.select()
+    return render_template("index.html", form=form, verbs=verbs)
+
